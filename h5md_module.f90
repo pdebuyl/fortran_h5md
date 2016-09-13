0025e4c12b829f71c27a46f9eafd67b5ae4c71e7
Pierre de Buyl <pdebuyl@pdebuyl.be>
2016-09-13 09:35:20 +0200

    add h5md_write_dataset_d4

--- h5md_module.f90
+++ h5md_module.f90
@@ -92,6 +92,7 @@ module h5md_module
      module procedure h5md_write_dataset_ds
      module procedure h5md_write_dataset_d2
      module procedure h5md_write_dataset_i2
+     module procedure h5md_write_dataset_d4
   end interface h5md_write_dataset
 
 contains
@@ -1588,6 +1589,27 @@ contains
     call h5sclose_f(s, error)
 
   end subroutine h5md_write_dataset_d2
+
+  subroutine h5md_write_dataset_d4(loc, name, value)
+    integer(HID_T), intent(inout) :: loc
+    character(len=*), intent(in) :: name
+    double precision, intent(in) :: value(:,:,:,:)
+
+    integer(HID_T) :: d, s, t
+    integer :: error
+    integer, parameter :: rank=4
+    integer(HSIZE_T) :: dims(rank)
+
+    t = H5T_NATIVE_DOUBLE
+    dims = shape(value)
+
+    call h5screate_simple_f(rank, dims, s, error)
+    call h5dcreate_f(loc, name, t, s, d, error)
+    call h5dwrite_f(d, t, value, dims, error)
+    call h5dclose_f(d, error)
+    call h5sclose_f(s, error)
+
+  end subroutine h5md_write_dataset_d4
 
   subroutine h5md_write_dataset_i2(loc, name, value)
     integer(HID_T), intent(inout) :: loc
